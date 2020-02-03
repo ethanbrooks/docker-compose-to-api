e729c2576e26b7189ba0fb19eb09db8858b022f5
ethanbrooks <ebrooks@hotelherrera.com>
2020-02-03 11:52:31 -0500

    Update to current versions

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,6 @@ const dockerComposeToApi = require('../lib/index');
 const tap = require('tap');
 const fs = require('fs');
 const path = require('path');
-const Joi = require('joi');
 const validator = require('joi-docker-engine');
 
 tap.test('dockerComposeToApi should be a function', (t) => {
@@ -12,35 +11,35 @@ tap.test('dockerComposeToApi should be a function', (t) => {
 });
 
 tap.test('dockerComposeToApi handles container_name', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.name.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.name.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
   t.deepEqual(engineSpec.Name, 'cool-auth');
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi can successfully convert an example schema', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.example.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.example.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi works with a passed in object', (t) => {
-  var engineSpec = dockerComposeToApi({
+  const engineSpec = dockerComposeToApi({
     auth: {
       image: 'test',
       container_name: 'cool-auth'
     }
   });
   t.deepEqual(engineSpec.Name, 'cool-auth');
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles network as object', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.network.object.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.network.object.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
   t.deepEqual(engineSpec.TaskTemplate.Networks, [
     {
       Target: 'first-network',
@@ -51,13 +50,13 @@ tap.test('dockerComposeToApi handles network as object', (t) => {
       Aliases: ['alias3', 'alias4']
     }
   ]);
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles network as array', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.network.array.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.network.array.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
   t.deepEqual(engineSpec.TaskTemplate.Networks, [
     {
       Target: 'first-network',
@@ -68,80 +67,80 @@ tap.test('dockerComposeToApi handles network as array', (t) => {
       Aliases: []
     }
   ]);
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles label as object', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.label.object.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
-  t.deepEqual(engineSpec.TaskTemplate.ContainerSpec.Labels, { 'test': 'one', 'fire': 'everything' });
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.label.object.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
+  t.deepEqual(engineSpec.TaskTemplate.ContainerSpec.Labels, { test: 'one', fire: 'everything' });
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles label as array', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.label.array.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
-  t.deepEqual(engineSpec.TaskTemplate.ContainerSpec.Labels, { 'test': 'one', 'fire': 'everything' });
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.label.array.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
+  t.deepEqual(engineSpec.TaskTemplate.ContainerSpec.Labels, { test: 'one', fire: 'everything' });
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles command as array', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.command.array.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.command.array.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
   t.deepEqual(engineSpec.TaskTemplate.ContainerSpec.Args, ['bundle', 'exec', 'npm', 'test']);
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles command as string', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.command.string.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.command.string.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
   t.deepEqual(engineSpec.TaskTemplate.ContainerSpec.Args, ['bundle exec npm test']);
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles environment variables as object', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.environment.object.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
-  t.deepEqual(engineSpec.TaskTemplate.ContainerSpec.Env, ['APP_NAME=micro-auth','REDIRECT_URL=/info', 'CACHE_LENGTH=100']);
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.environment.object.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
+  t.deepEqual(engineSpec.TaskTemplate.ContainerSpec.Env, ['APP_NAME=micro-auth', 'REDIRECT_URL=/info', 'CACHE_LENGTH=100']);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles environment variables as array', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.environment.array.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
-  t.deepEqual(engineSpec.TaskTemplate.ContainerSpec.Env, ['APP_NAME=micro-auth','REDIRECT_URL=/info']);
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.environment.array.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
+  t.deepEqual(engineSpec.TaskTemplate.ContainerSpec.Env, ['APP_NAME=micro-auth', 'REDIRECT_URL=/info']);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles deploy', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.deploy.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.deploy.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
   t.deepEqual(engineSpec, {
     Labels: {
-      "one": "two",
-      "three": "four"
+      one: 'two',
+      three: 'four'
     },
     Mode: {
       Replicated: { Replicas: 6 }
     },
-    Name: "auth",
+    Name: 'auth',
     EndpointSpec: {
-      Mode: "vip"
+      Mode: 'vip'
     },
     TaskTemplate: {
       ContainerSpec: {
-        Image: "firstandthird/node:8.9-2-dev"
+        Image: 'firstandthird/node:8.9-2-dev'
       },
       Placement: {
         Constraints: ['node.role == manager', 'engine.labels.operatingsystem == ubuntu 14.04'],
-        Preferences: [{ Spread: { SpreadDescriptor: 'node.labels.zone' }}]
+        Preferences: [{ Spread: { SpreadDescriptor: 'node.labels.zone' } }]
       },
       Resources: {
         Limits: {
@@ -169,51 +168,51 @@ tap.test('dockerComposeToApi handles deploy', (t) => {
       Order: 'stop-first'
     }
   });
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles array syntax for volume', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.volume.array.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.volume.array.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
   t.deepEqual(engineSpec.TaskTemplate.ContainerSpec.Mounts, [
     {
-      Source: ".",
-      Target: "/home/app/src",
-      Type: "bind",
+      Source: '.',
+      Target: '/home/app/src',
+      Type: 'bind',
     },
     {
       ReadOnly: true,
-      Source: "./test",
-      Target: "/home/app/test",
-      Type: "bind"
+      Source: './test',
+      Target: '/home/app/test',
+      Type: 'bind'
     },
     {
-      Source: "/home/app/static",
-      Type: "bind"
+      Source: '/home/app/static',
+      Type: 'bind'
     }
   ]);
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles object syntax for volume', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.volume.object.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.volume.object.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
   t.deepEqual(engineSpec.TaskTemplate.ContainerSpec.Mounts, [
     {
-      Source: ".",
-      Target: "/home/app/src",
-      Type: "volume",
+      Source: '.',
+      Target: '/home/app/src',
+      Type: 'volume',
       ReadOnly: false,
       VolumeOptions: {
         NoCopy: false
       }
     },
     {
-      Source: "./static",
-      Target: "/home/app/static",
-      Type: "bind",
+      Source: './static',
+      Target: '/home/app/static',
+      Type: 'bind',
       BindOptions: {
         Propagation: 'shared'
       }
@@ -226,13 +225,13 @@ tap.test('dockerComposeToApi handles object syntax for volume', (t) => {
       }
     }
   ]);
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles array syntax for port', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.port.array.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.port.array.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
   t.deepEqual(engineSpec.EndpointSpec.Ports, [
     {
       Protocol: 'tcp',
@@ -249,13 +248,13 @@ tap.test('dockerComposeToApi handles array syntax for port', (t) => {
       PublishedPort: 5000
     }
   ]);
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
 
 tap.test('dockerComposeToApi handles object syntax for port', (t) => {
-  var yaml = fs.readFileSync(path.join(__dirname, 'fixtures' , 'service.port.object.yml'), { encoding: 'utf8'});
-  var engineSpec = dockerComposeToApi(yaml);
+  const yaml = fs.readFileSync(path.join(__dirname, 'fixtures', 'service.port.object.yml'), { encoding: 'utf8' });
+  const engineSpec = dockerComposeToApi(yaml);
   t.deepEqual(engineSpec.EndpointSpec.Ports, [
     {
       Protocol: 'tcp',
@@ -274,6 +273,6 @@ tap.test('dockerComposeToApi handles object syntax for port', (t) => {
       PublishMode: 'ingress'
     }
   ]);
-  t.equal(Joi.validate(engineSpec, validator).error, null);
+  t.equal(validator.validate(engineSpec).error, null);
   t.end();
 });
